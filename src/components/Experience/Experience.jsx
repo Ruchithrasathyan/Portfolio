304d953bf515fda878d871202796594f45f00c6c
Ruchithrasathyan <sathyanarayanan.r@northeastern.edu>
2024-02-05 19:32:20 -0500

    added @

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from "./Experience.module.css";
-import history from "../../data/history.json";
-import { getImageUrl } from '../../utils';
+import history from "@/data/history.json";
+import { getImageUrl } from '@/utils';
 // import { Skills } from '../Skills/Skills';
 
 export const Experience = () => {
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import skills from "../../data/skills.json";
-import { getImageUrl } from '../../utils';
+import skills from "@/data/skills.json";
+import { getImageUrl } from '@/utils';
 import styles from "./Skills.module.css";
 
 export const Skills = () => {
